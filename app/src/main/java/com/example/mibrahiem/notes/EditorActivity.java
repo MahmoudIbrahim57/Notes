9473816ca602009eb7cc44c4a2afa0abf5265697
Mahmoud Ibrahim <mahmoud.ibrahim@codexit.net>
2019-02-11 01:50:20 +0200

    finish SpectrumPlatte lesson 2

--- app/src/main/java/com/example/mibrahiem/notes/EditorActivity.java
+++ app/src/main/java/com/example/mibrahiem/notes/EditorActivity.java
@@ -1,6 +1,7 @@
 package com.example.mibrahiem.notes;
 
 import android.app.ProgressDialog;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,8 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.thebluealliance.spectrum.SpectrumPalette;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -20,12 +23,28 @@ public class EditorActivity extends AppCompatActivity {
 EditText et_title, et_note;
 ProgressDialog progressDialog;
 ApiInterface apiInterface;
+SpectrumPalette palette;
+int color;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
         et_title=findViewById(R.id.title);
         et_note=findViewById(R.id.note);
+        palette=findViewById(R.id.palette);
+
+        //chose color
+        palette.setOnColorSelectedListener(
+                clr -> color = clr
+        );
+
+        // default color
+
+        palette.setSelectedColor(getResources().getColor(R.color.white));
+        color = getResources().getColor(R.color.white);
+
+        //progress dialog
+
         progressDialog =new ProgressDialog(this);
         progressDialog.setMessage("Please Wait .");
     }
@@ -45,7 +64,7 @@ ApiInterface apiInterface;
             case R.id.save :
                 String title=et_title.getText().toString().trim();
                 String note=et_note.getText().toString().trim();
-                int color = -2184710;
+                int color = this.color;
                 if(title.isEmpty())
                 {
                     et_title.setError("Enter The Title");
